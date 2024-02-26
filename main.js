5adfe0344e7daa4541ad7fbf74bba17ec5bc2c89
Edwing Muñoz <eamc201168@gmail.com>
2024-02-26 16:56:24 -0500

    Nest elements in their containers and set their classes

--- main.js
+++ main.js
@@ -9,23 +9,28 @@ function clean() {
 
 submit.addEventListener('click', function (){
     clean();
-
-    const botones = document.querySelector('.rating');
+    //Container
     const post = document.createElement('div');
+    post.className = 'thanksSection';
+    //Ilustration
     const img = document.createElement('img');
     img.setAttribute('src', 'images/illustration-thank-you.svg')
-    post.appendChild(img);
 
+    //section that contains the info of selection
     const selection = document.createElement('p');
-    const message = document.createTextNode("You've made a selection");
-    selection.append(message);
-
-
+    const selDiv = document.createElement('section');
+    selection.append("You've made a selection");
+    //add class to section
+    selDiv.appendChild(selection);
+    selDiv.className = 'sel';
+    //'Thank you' header
     const thanks = document.createElement('h1');
     thanks.append("Thank you!")
-
+    //final message
     const finalMessage = document.createElement('p');
+    finalMessage.className = 'finalMsg';
     finalMessage.append("We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!")
 
-    main.append(post,selection,thanks, finalMessage);
+    post.append(img, selDiv, thanks, finalMessage);
+    main.append(post);
 })
